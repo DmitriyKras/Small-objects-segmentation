e4cf98bb6ef3915a24a4e4a2ddd61f49773f2612
Dmitriy <116486846+DmitriyKras@users.noreply.github.com>
2023-03-03 17:11:08 +0300

    Update PSP_Net_model.py

--- PSP_Net/PSP_Net_model.py
+++ PSP_Net/PSP_Net_model.py
@@ -4,59 +4,44 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Dens
 from tensorflow.keras.layers import BatchNormalization, Activation, Dropout, ZeroPadding2D, Lambda, Conv3D, MaxPool3D
 from tensorflow.keras.layers import Concatenate, Add, Layer, Multiply, Reshape
 import tensorflow as tf
-from tensorflow.keras.optimizers import Adam, SGD
-import tensorflow.keras.backend as K
 
 
-
-
-def BN(name=""):
-    """
-    Batch normalization layer
-    """
+def BN():
     return BatchNormalization(momentum=0.95, name=name, epsilon=1e-5)
 
 
-def residual_short(prev_layer, level, pad=1, lvl=1, sub_lvl=1, modify_stride=False):
+def residual_short(prev_layer, level, pad=1, modify_stride=False):
     """
     Residual short used in the beggining of each lvl
     """
     prev_layer = Activation('relu')(prev_layer)
     block_1 = residual_conv(prev_layer, level,
-                            pad=pad, lvl=lvl, sub_lvl=sub_lvl,
-                            modify_stride=modify_stride)
+                            pad=pad, modify_stride=modify_stride)
 
     block_2 = short_convolution_branch(prev_layer, level,
-                                       lvl=lvl, sub_lvl=sub_lvl,
                                        modify_stride=modify_stride)
     added = Add()([block_1, block_2])
     return added
 
 
-def residual_empty(prev_layer, level, pad=1, lvl=1, sub_lvl=1):
+def residual_empty(prev_layer, level, pad=1):
     prev_layer = Activation('relu')(prev_layer)
 
-    block_1 = residual_conv(prev_layer, level, pad=pad,
-                            lvl=lvl, sub_lvl=sub_lvl)
+    block_1 = residual_conv(prev_layer, level, pad=pad)
     block_2 = empty_branch(prev_layer)
     added = Add()([block_1, block_2])
     return added
 
 
-def short_convolution_branch(prev, level, lvl=1, sub_lvl=1, modify_stride=False):
-    lvl = str(lvl)
-    sub_lvl = str(sub_lvl)
-    names = ["conv" + lvl + "_" + sub_lvl + "_1x1_proj",
-             "conv" + lvl + "_" + sub_lvl + "_1x1_proj_bn"]
-
-    if modify_stride is False:
-        prev = Conv2D(256 * level, (1, 1), strides=(1, 1), name=names[0],
+def short_convolution_branch(prev, level, modify_stride=False):
+    if modify_stride == False:
+        prev = Conv2D(256 * level, (1, 1), strides=(1, 1),
                       use_bias=False)(prev)
-    elif modify_stride is True:
-        prev = Conv2D(256 * level, (1, 1), strides=(2, 2), name=names[0],
+    elif modify_stride == True:
+        prev = Conv2D(256 * level, (1, 1), strides=(2, 2),
                       use_bias=False)(prev)
 
-    prev = BN(name=names[1])(prev)
+    prev = BN()(prev)
     return prev
 
 
@@ -64,34 +49,26 @@ def empty_branch(prev):
     return prev
 
 
-def residual_conv(prev, level, pad=1, lvl=1, sub_lvl=1, modify_stride=False):
-    lvl = str(lvl)
-    sub_lvl = str(sub_lvl)
-    names = ["conv" + lvl + "_" + sub_lvl + "_1x1_reduce",
-             "conv" + lvl + "_" + sub_lvl + "_1x1_reduce_bn",
-             "conv" + lvl + "_" + sub_lvl + "_3x3",
-             "conv" + lvl + "_" + sub_lvl + "_3x3_bn",
-             "conv" + lvl + "_" + sub_lvl + "_1x1_increase",
-             "conv" + lvl + "_" + sub_lvl + "_1x1_increase_bn"]
+def residual_conv(prev, level, pad=1, modify_stride=False):
     if modify_stride is False:
-        prev = Conv2D(64 * level, (1, 1), strides=(1, 1), name=names[0],
+        prev = Conv2D(64 * level, (1, 1), strides=(1, 1),
                       use_bias=False)(prev)
     elif modify_stride is True:
-        prev = Conv2D(64 * level, (1, 1), strides=(2, 2), name=names[0],
+        prev = Conv2D(64 * level, (1, 1), strides=(2, 2),
                       use_bias=False)(prev)
 
-    prev = BN(name=names[1])(prev)
+    prev = BN()(prev)
     prev = Activation('relu')(prev)
 
     prev = ZeroPadding2D(padding=(pad, pad))(prev)
     prev = Conv2D(64 * level, (3, 3), strides=(1, 1), dilation_rate=pad,
-                  name=names[2], use_bias=False)(prev)
-
-    prev = BN(name=names[3])(prev)
-    prev = Activation('relu')(prev)
-    prev = Conv2D(256 * level, (1, 1), strides=(1, 1), name=names[4],
                   use_bias=False)(prev)
-    prev = BN(name=names[5])(prev)
+
+    prev = BN()(prev)
+    prev = Activation('relu')(prev)
+    prev = Conv2D(256 * level, (1, 1), strides=(1, 1),
+                  use_bias=False)(prev)
+    prev = BN()(prev)
     return prev
 
 
@@ -118,37 +95,29 @@ class Interp(Layer):
         return config
     
 
-def ResNet_with_different_level_features(inp, layers):
+def ResNet_with_different_level_features(inp):
     
     # inp - Input layer
-    # Names for the first couple layers of model
-    names = ["conv1_1_3x3_s2",
-             "conv1_1_3x3_s2_bn",
-             "conv1_2_3x3",
-             "conv1_2_3x3_bn",
-             "conv1_3_3x3",
-             "conv1_3_3x3_bn"]
-
     layers_to_merge = []  # this layers will be concatenated
 
     # Short branch(only start of network)
 
     cnv1 = Conv2D(64, (3, 3), strides=(2, 2), padding='same', 
-                  name=names[0], use_bias=False)(inp)  # "conv1_1_3x3_s2"
-    bn1 = BN(name=names[1])(cnv1)  # "conv1_1_3x3_s2/bn"
-    relu1 = Activation('relu')(bn1)  # "conv1_1_3x3_s2/relu"
+                  use_bias=False)(inp) 
+    bn1 = BN()(cnv1)
+    relu1 = Activation('relu')(bn1)
 
     cnv1 = Conv2D(64, (3, 3), strides=(1, 1), padding='same', 
-                  name=names[2], use_bias=False)(relu1)  # "conv1_2_3x3"
-    bn1 = BN(name=names[3])(cnv1)  # "conv1_2_3x3/bn"
-    relu1 = Activation('relu')(bn1)  # "conv1_2_3x3/relu"
+                  use_bias=False)(relu1)
+    bn1 = BN()(cnv1)
+    relu1 = Activation('relu')(bn1)
 
     cnv1 = Conv2D(128, (3, 3), strides=(1, 1), padding='same', 
-                  name=names[4], use_bias=False)(relu1)  # "conv1_3_3x3"
-    bn1 = BN(name=names[5])(cnv1)  # "conv1_3_3x3/bn"
-    relu1 = Activation('relu')(bn1)  # "conv1_3_3x3/relu"
+                 use_bias=False)(relu1)
+    bn1 = BN()(cnv1)
+    relu1 = Activation('relu')(bn1)
 
-    res = MaxPooling2D(pool_size=(3, 3), padding='same', strides=(2, 2))(relu1)  # "pool1_3x3_s2"
+    res = MaxPooling2D(pool_size=(3, 3), padding='same', strides=(2, 2))(relu1)
 
     layers_to_merge.append(res)  # first part of the network will be concatenated with all next
 
@@ -160,33 +129,30 @@ def ResNet_with_different_level_features(inp, layers):
     """
 
     # 2_1- 2_3
-    res = residual_short(res, 1, pad=1, lvl=2, sub_lvl=1)
+    res = residual_short(res, 1, pad=1)
     for i in range(2):
-        res = residual_empty(res, 1, pad=1, lvl=2, sub_lvl=i + 2)
+        res = residual_empty(res, 1, pad=1)
 
     layers_to_merge.append(res)
 
     # 3_1 - 3_3
-    res = residual_short(res, 2, pad=1, lvl=3, sub_lvl=1, modify_stride=True)
+    res = residual_short(res, 2, pad=1, modify_stride=True)
     for i in range(3):
-        res = residual_empty(res, 2, pad=1, lvl=3, sub_lvl=i + 2)
+        res = residual_empty(res, 2, pad=1)
 
     layers_to_merge.append(res)
 
-    if layers is 50:
-        # 4_1 - 4_6
-        res = residual_short(res, 4, pad=2, lvl=4, sub_lvl=1)
-        for i in range(5):
-            res = residual_empty(res, 4, pad=2, lvl=4, sub_lvl=i + 2)
-    else:
-        print("This ResNet is not implemented")
+    # 4_1 - 4_6
+    res = residual_short(res, 4, pad=2, lvl=4, sub_lvl=1)
+    for i in range(5):
+        res = residual_empty(res, 4, pad=2, lvl=4, sub_lvl=i + 2)
 
     layers_to_merge.append(res)
 
     # 5_1 - 5_3
-    res = residual_short(res, 8, pad=4, lvl=5, sub_lvl=1)
+    res = residual_short(res, 8, pad=4)
     for i in range(2):
-        res = residual_empty(res, 8, pad=4, lvl=5, sub_lvl=i + 2)
+        res = residual_empty(res, 8, pad=4)
 
     res = Activation('relu')(res)
 
@@ -216,31 +182,22 @@ def interp_block(prev_layer, level, feature_map_shape, input_shape):
                               2: 30,
                               3: 20,
                               6: 10}
-    elif input_shape == (713, 713):
-        kernel_strides_map = {1: 90,
-                              2: 45,
-                              3: 30,
-                              6: 15}
     else:
         print("Pooling parameters for input shape ",
               input_shape, " are not defined.")
         exit(1)
 
-    names = [
-        "conv5_3_pool" + str(level) + "_conv",
-        "conv5_3_pool" + str(level) + "_conv_bn"
-    ]
+
     kernel = (kernel_strides_map[level], kernel_strides_map[level])
     strides = (kernel_strides_map[level], kernel_strides_map[level])
     prev_layer = AveragePooling2D(kernel, strides=strides)(prev_layer)
-    prev_layer = Conv2D(512, (1, 1), strides=(1, 1), name=names[0],
+    prev_layer = Conv2D(512, (1, 1), strides=(1, 1),
                         use_bias=False)(prev_layer)
-    prev_layer = BN(name=names[1])(prev_layer)
+    prev_layer = BN()(prev_layer)
     prev_layer = Activation('relu')(prev_layer)
-    # prev_layer = Lambda(Interp, arguments={
-    #                    'shape': feature_map_shape})(prev_layer)
     prev_layer = Interp(feature_map_shape)(prev_layer)
     return prev_layer
+
 
 def build_pyramid_pooling_module(res, input_shape):
     """Build the Pyramid Pooling Module."""
@@ -263,17 +220,17 @@ def build_pyramid_pooling_module(res, input_shape):
     return res
   
   
-def CPA_net(nb_classes, resnet_layers, input_shape, activation='softmax'):
+def CPA_net(nb_classes, input_shape):
     
     inp = Input((input_shape[0], input_shape[1], 3))
 
-    res = ResNet_with_different_level_features(inp, layers=resnet_layers)
+    res = ResNet_with_different_level_features(inp)
 
     psp = build_pyramid_pooling_module(res, input_shape)
 
-    xx = Conv2D(512, (3, 3), strides=(1, 1), padding="same", name="conv5_4",
+    xx = Conv2D(512, (3, 3), strides=(1, 1), padding="same",
                use_bias=False)(psp)
-    xx = BN(name="conv5_4_bn")(xx)
+    xx = BN()(xx)
     xx = Activation('relu')(xx)
     xx = Dropout(0.1)(xx)
 
@@ -290,14 +247,14 @@ def CPA_net(nb_classes, resnet_layers, input_shape, activation='softmax'):
 
     # pixel attention starts
     pixel_weighting = Conv2D(1024, (3, 3), strides=(1, 1), padding="same", use_bias=False)(xx)
-    pixel_weighting = BN(name="pixel_attention_conv_bn_1")(pixel_weighting)
+    pixel_weighting = BN()(pixel_weighting)
     pixel_weighting = Activation('relu')(pixel_weighting)
     pixel_weighting = Dropout(0.1)(pixel_weighting)
     pixel_weighting = Conv2D(1024, (3, 3), strides=(1, 1), padding="same", use_bias=False)(pixel_weighting)
-    pixel_weighting = BN(name="pixel_attention_conv_bn_2")(pixel_weighting)
+    pixel_weighting = BN()(pixel_weighting)
     pixel_weighting = Activation('relu')(pixel_weighting)
     pixel_weighting = Conv2D(512, (1, 1), strides=(1, 1), padding="same", use_bias=False)(pixel_weighting)
-    pixel_weighting = BN(name="pixel_attention_conv_bn_3")(pixel_weighting)
+    pixel_weighting = BN()(pixel_weighting)
     pixel_weighting = Activation('relu')(pixel_weighting)
     pixel_weighting = Dropout(0.1)(pixel_weighting)
     pixel_weighting = Conv2D(2, (1, 1))(pixel_weighting)
